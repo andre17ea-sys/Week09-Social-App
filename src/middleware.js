d12bd9b1e88895b6ca7a6057b3ad1c893c7538b6
ANDREEA TOMA <andre17ea@gmail.com>
2025-11-02 21:59:14 +0000

    fixed middleware after deployed fail

--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,12 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// export default middleware function ///  check authentication and protect routes if needed
-
+// export default middleware function - check authentication and protect routes if needed
 export default clerkMiddleware();
 
 // config object to define which routes middleware applies to
 export const config = {
   matcher: [
-    // skip next.js internals and static files
+    // skip Next.js internals and static files
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // always run for API routes
     "/(api|trpc)(.*)",
